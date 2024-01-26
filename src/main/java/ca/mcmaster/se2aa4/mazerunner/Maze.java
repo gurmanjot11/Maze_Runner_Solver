23fc50b0bf6edc47ce84a43de080d38980a4dab4
gurmanjot <gurmanjotminhas@gmail.com>
2024-01-26 15:27:21 -0500

    Refactored Code, Walking Skeleton

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -0,0 +1,14 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class Maze {
+    String[][] maze_self;
+
+    public Maze(String[][] maze_array){
+        this.maze_self=maze_array;
+    }
+
+    public String solveMaze(){
+        return "No path yet";
+    }
+}
+
