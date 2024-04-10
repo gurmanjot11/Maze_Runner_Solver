1dd909e156007b96a5ab65ab24f0075eda68305d
gurmanjot <gurmanjotminhas@gmail.com>
2024-04-09 22:48:22 -0400

    readded verification functionality #1

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -14,7 +14,7 @@ import org.apache.commons.cli.*;
 public class Main {
     private static final Logger logger = LogManager.getLogger();
 
-    private record Congifuration (String maze_filepath){
+    private record Congifuration (String maze_filepath, String test_path){
         Congifuration{
             if (maze_filepath == null){
                 throw new IllegalArgumentException("File path to maze cannot be null");
@@ -27,6 +27,8 @@ public class Main {
 
         /** Option for taking input maze file path */
         options.addOption("i","input", true, "input file for maze");
+        options.addOption("p","path", true, "Optional flag to provide test path for maze");
+
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
         
@@ -37,9 +39,11 @@ public class Main {
         }
 
         String  maze_filename= cmd.getOptionValue("i");
+        String test_path = cmd.getOptionValue("p");
+
         logger.info("**** Received input maze file "+ maze_filename);
 
-        return new Congifuration(maze_filename);
+        return new Congifuration(maze_filename,test_path);
     }
 
     public static void main(String[] args) {
@@ -47,9 +51,22 @@ public class Main {
             Congifuration config = configure(args);
             try {
                 Maze maze= new Maze(config.maze_filepath);
-                MazeSolver maze_solver = new RightHandRunner();
-                Path path = maze_solver.solveMaze(maze);
-                System.out.println("Path: "+ path.getFactorizedPath());
+                if(config.test_path!=null){
+                    Path path= new Path(config.test_path);
+                    path=path.defactorPath();
+                    boolean valid=path.verifyPath(maze);
+                    if (valid){
+                        System.out.println("\n**This path is valid**");
+                    }
+                    else{
+                        System.out.println("\n**This path is NOT valid**");
+                    }
+                }
+                else{
+                    MazeSolver maze_solver = new RightHandRunner();
+                    Path path = maze_solver.solveMaze(maze);
+                    System.out.println("Path: "+ path.getFactorizedPath());
+                }
             } catch (IOException ioe) {
                 logger.error("/!\\ Error Reading File /!\\");
                 logger.error("/!\\ Terminating Program /!\\");
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -1,8 +1,12 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Path {
     String path_raw;
-  
+    private static final Logger logger = LogManager.getLogger();
+
     public Path(String path){
         this.path_raw=path;
     }  
@@ -39,6 +43,108 @@ public class Path {
         } 
 
         return f_path;
+
+    }
+    /**Verifies only valid characters are passed in the path */
+    private boolean ensureCharactersValid(){
+        for (int i=0; i<path_raw.length();i++){
+            if (path_raw.charAt(i)=='R' | path_raw.charAt(i)=='F' | path_raw.charAt(i)=='L'| path_raw.charAt(i)==' '){
+                continue;
+            }
+            else{
+                try {
+                    Integer.parseInt(String.valueOf(path_raw.charAt(i)));
+                }
+                catch(NumberFormatException nfe){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public Path defactorPath(){
+        //assume valid chars
+        if (!ensureCharactersValid()){
+            logger.error("/!\\ Invalid path given /!\\");
+            logger.error("/!\\ Ensure all capitals and only R,F,L /!\\");
+            logger.error("/!\\ Terminating Program /!\\");
+            System.exit(1);
+        }
+        String path_defactored="";
+        // cases (assuming valid input: factored path: 3F4R2F -> [3F,4R,2F], canonical: FFFRFFF
+        //Case canonical path passed to validate, will return a spaceless path which can be analyzed
+        if (path_raw.charAt(0) == 'F' | path_raw.charAt(0) == 'R' | path_raw.charAt(0) == 'L'){
+            for (int i=0; i<path_raw.length();i++){
+                if (!(path_raw.charAt(i) == 'F' | path_raw.charAt(i) == 'R' | path_raw.charAt(i) == 'L')){
+                    logger.error("/!\\ Invalid path given /!\\");
+                    logger.error("/!\\ Ensure path is fully factorized or canonical w/o spaces /!\\");
+                    logger.error("/!\\ Terminating Program /!\\");
+                    System.exit(1);
+                }
+            }
+            return new Path(this.path_raw);
+        }
+        //Case factorized path provided, will split at each different instruction and then create corresponding spaceless canonical path
+        else{
+            String[] factors = path_raw.split("(?=\\d)(?<!\\d)"); //splits after every int_str pattern
+            for (int i=0; i<factors.length;i++){
+                try{
+                    Integer.parseInt(factors[i].substring(0,factors[i].length()-1));
+                }
+                catch(NumberFormatException nfe){
+                    logger.error("/!\\ Invalid path given /!\\");
+                    logger.error("/!\\ Ensure path is fully formatted as canonical or factorized w/o spaces /!\\");
+                    logger.error("/!\\ Terminating Program /!\\");
+                    System.exit(1);
+                }
+                int fac=Integer.parseInt(factors[i].substring(0,factors[i].length()-1));
+                char direction= (factors[i].charAt(factors[i].length()-1));
+                for (int j=0; j<fac;j++){
+                    path_defactored+=direction;
+                }
+            }
+            path_raw=path_defactored;
+            return new Path(this.path_raw);
+        }
+    }
+    public int getPathLength(){
+        return (this.path_raw).length();
+    }
+    public char getStepAt(int i){
+        return path_raw.charAt(i);
+    }
+    public boolean verifyPath(Maze maze){
+        logger.info("Verifying path from western entry...");
+        Coordinates west_entry=maze.getWestEntry();
+        Coordinates east_entry=maze.getEastEntry();
+        Coordinates pos=west_entry;
+        DIRECTION direct= DIRECTION.EAST;
+        for (int i=0; i<this.getPathLength();i++){
+            char move=this.getStepAt(i);
+            switch(move){
+                case 'F' ->{
+                    pos.move(direct);
+                }
+                case 'L' ->{
+                    direct=direct.rotateLeft();
+                }
+                case 'R' ->{
+                    direct=direct.rotateRight();
+                }
+                default -> {
+                    return false;
+                }
+            }
+            if(!maze.isOpen(pos)){
+                return false;
+            }
+        }
+        if (pos.coordEquals(east_entry)){
+            return true;
+        }
+        else{
+            return false;
+        }
 
     }
 
