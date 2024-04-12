13adbb14b68a04a2216997dd4ca6474632a0fe93
gurmanjot <gurmanjotminhas@gmail.com>
2024-04-12 16:47:35 -0400

    Created a way to extract a path from the BFS index #5

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/GraphMazeRunner.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/GraphMazeRunner.java
@@ -1,16 +1,29 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Map;
 import java.util.Stack;
 
 public class GraphMazeRunner implements MazeSolver{
+    private static final Logger logger = LogManager.getLogger();
+
     @Override
     public Path solveMaze(Maze maze) {
         Graph g = convertToUndirectedGraph(maze);
         //g.printGraph();
-        Map<CoordinatesVertex,CoordinatesVertex> index = g.breadthFirstSearch(new CoordinatesVertex(maze.getWestEntry()), new CoordinatesVertex(maze.getEastEntry()));
-        extractPath(index,maze.getWestEntry(),maze.getEastEntry());
-        Path p = new Path("FFFF");
+        Map<CoordinatesVertex,CoordinatesVertex> index;
+        try{
+            index = g.breadthFirstSearch(new CoordinatesVertex(maze.getWestEntry()), new CoordinatesVertex(maze.getEastEntry()));
+            extractPath(index,maze.getWestEntry(),maze.getEastEntry());
+        }
+        catch(RuntimeException rte){
+            logger.error("Invalid maze or start/end parameters");
+            System.exit(1);
+        }
+        index = g.breadthFirstSearch(new CoordinatesVertex(maze.getWestEntry()), new CoordinatesVertex(maze.getEastEntry()));
+        Path p = extractPath(index,maze.getWestEntry(),maze.getEastEntry());
         return p;
     }
     private Graph convertToUndirectedGraph(Maze maze){
@@ -34,18 +47,56 @@ public class GraphMazeRunner implements MazeSolver{
         }
         return g;
     }
-    private void extractPath(Map<CoordinatesVertex,CoordinatesVertex> index,Coordinates start, Coordinates end){
+    private Path extractPath(Map<CoordinatesVertex,CoordinatesVertex> index,Coordinates start, Coordinates end){
         Stack<Character> instructions = new Stack<>();
+        DIRECTION direction = DIRECTION.WEST;
 
-        CoordinatesVertex cv = index.get(new CoordinatesVertex(end));
-        while(!cv.equals(new CoordinatesVertex(start))){
-            System.out.print(cv.getLabel()+"->");
-            cv = index.get(cv);
+        CoordinatesVertex cv_curr = new CoordinatesVertex(end);
+        CoordinatesVertex cv_prev = index.get(cv_curr);
+        while(!cv_curr.equals(new CoordinatesVertex(start))){
+            String movement = determineSequence(cv_prev.getObject(),cv_curr.getObject(),direction);
+            //System.out.print(movement);
+            if(movement.charAt(0)=='R'){
+                direction = direction.rotateRight();
+            }
+            else if (movement.charAt(0)=='L') {
+                direction = direction.rotateLeft();
+            }
+            cv_curr=cv_prev;
+            cv_prev = index.get(cv_prev);
+            for (int i=0; i<movement.length();i++){
+                if(movement.charAt(i)=='L'){
+                    instructions.add('R');
+                }
+                else if (movement.charAt(i)=='R') {
+                    instructions.add('L');
+                }
+                else{
+                    instructions.add('F');
+                }
+            }
         }
-        System.out.print(cv.getLabel());
-
+        String path="";
+        while (!instructions.isEmpty()){
+            path+=instructions.pop();
+        }
+        return new Path(path);
     }
-//    private String determineSequence(Coordinates prev_coord, Coordinates curr_coord){
-//
-//    }
+    private String determineSequence(Coordinates prev_coord, Coordinates curr_coord, DIRECTION curr_direction){
+        Coordinates fwd_coord = curr_coord.preMoveCheck(curr_direction);
+        Coordinates left_coord = curr_coord.preMoveCheck(curr_direction.rotateLeft());
+        Coordinates right_coord = curr_coord.preMoveCheck(curr_direction.rotateRight());
+        if(fwd_coord.coordEquals(prev_coord)){
+            return "F";
+        }
+        else if (left_coord.coordEquals(prev_coord)){
+            return "LF";
+        }
+        else if (right_coord.coordEquals(prev_coord)){
+            return "RF";
+        }
+        else{
+            throw new RuntimeException("bad path");
+        }
+    }
 }
